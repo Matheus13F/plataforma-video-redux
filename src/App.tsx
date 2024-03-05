2b71c2572196ba4a8c87b12c936c17b212b96a48
matheus13f <matheus-goes13@hotmail.com>
2024-03-05 10:25:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { Player } from "./pages/Player";
+
+export function App() {
+  return (
+    <Provider store={store}>
+      <Player />
+    </Provider>
+  );
+}
